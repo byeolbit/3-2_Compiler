5864c259e5f3e2b18360fbec674503c2f15c2092
Sang-Gyueong Jo <info@byeolbit.com>
2016-09-12 02:42:11 +0900

    Implement writeCFile function

--- ADCompiler/src/ADCompiler/CWriter.java
+++ ADCompiler/src/ADCompiler/CWriter.java
@@ -1,5 +1,7 @@
 package ADCompiler;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.List;
 
 /**
@@ -11,5 +13,23 @@ class CWriter {
     }
 
     void writeCFile(List<String> convertedCode) {
+
+        try {
+            BufferedWriter fw = new BufferedWriter(new FileWriter("./test.c", true));
+
+            for (String aConvertedCode : convertedCode) {
+                fw.write(aConvertedCode);
+                fw.flush();
+            }
+
+            fw.close();
+
+        } catch (Exception e) {
+
+            System.err.format("File write error : \n %s", e);
+            System.exit(1);
+
+        }
+
     }
 }
