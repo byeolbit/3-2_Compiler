7fe6e78e9e1e35c3d2f4b17e7c671f1ffb243de8
Sang-Gyueong Jo <info@byeolbit.com>
2016-09-12 02:15:28 +0900

    Implement source converter

---
+++ ADCompiler/src/ADCompiler/SourceConverter.java
@@ -0,0 +1,81 @@
+package ADCompiler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by josanggyeong on 2016. 9. 11..
+ */
+class SourceConverter {
+
+    private final List<String> convertedCode = new ArrayList<>();
+    private String convertedListId = "";
+    private String convertedResultId = "";
+
+    SourceConverter() {
+        convertedCode.add("#include<stdio.h>");
+        convertedCode.add("int main(){");
+    }
+
+    List<String> getConvertedCode() {
+        return convertedCode;
+    }
+
+    void convertCode(String type, List<String> originalCode) {
+
+
+        switch (type) {
+
+            case "DEF":
+
+                Pattern listPattern = Pattern.compile("\\[(.*?)\\]");
+                Matcher m = listPattern.matcher(originalCode.get(1));
+                String convertedList = null;
+                while (m.find()) convertedList = m.group(1);
+                convertedListId = "_AD_" + originalCode.get(0);
+                int listLength = 0;
+                if (convertedList != null) {
+                    listLength = convertedList.split(", ").length;
+                } else {
+                    System.err.format("Syntax error\n");
+                    System.exit(1);
+                }
+
+                convertedCode.add("\n");
+                convertedCode.add("\tlong " + convertedListId + "[] = {" + convertedList + "};");
+                convertedCode.add("\tint _AD_list_size = " + listLength + ";");
+
+                break;
+
+            case "REDUCE":
+
+                String[] originalCodes = originalCode.get(1).split(" ");
+
+                String convertedOperator = originalCodes[0];
+                //String convertedInteger = originalCodes[1];
+                convertedResultId = "_AD_result";
+
+                convertedCode.add("\tlong " + convertedResultId + " = 0;");
+                convertedCode.add("\n");
+                convertedCode.add("\tfor (_AD_i = 0 ; _AD_i < _AD_size ; _AD_i++){");
+                convertedCode.add("\t\t" + convertedResultId + " " + convertedOperator + "= " + convertedListId + "[_AD_i];");
+                convertedCode.add("\t}");
+                break;
+
+            case "PRINT":
+
+                convertedCode.add("\tprintf(" + '"' + "%d" + '"' + "," + convertedResultId + ");");
+
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
+
+}
