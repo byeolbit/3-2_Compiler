2033e2266e980329e6c6755b8422b21123f0d161
Sang-Gyueong Jo <info@byeolbit.com>
2016-09-12 02:13:56 +0900

    Init class

---
+++ ADCompiler/src/ADCompiler/CWriter.java
@@ -0,0 +1,15 @@
+package ADCompiler;
+
+import java.util.List;
+
+/**
+ * Created by josanggyeong on 2016. 9. 12..
+ */
+class CWriter {
+
+    CWriter() {
+    }
+
+    void writeCFile(List<String> convertedCode) {
+    }
+}
