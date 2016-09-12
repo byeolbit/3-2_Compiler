305670e86644dbe58338af7247c9b1df332b0d36
Byeolbit <info@byeolbit.com>
2016-09-12 10:56:38 +0900

    Add project explanation

--- ADCompiler/src/ADCompiler/Main.java
+++ ADCompiler/src/ADCompiler/Main.java
@@ -1,5 +1,13 @@
 package ADCompiler;
 
+/**
+ * ADCompiler
+ * Created by josanggyeong on 2016. 9. 10..
+ * <p>
+ * Used OS : Mac OS X 10.11.6
+ * Tested C compiler : Clang-703.0.31
+ */
+
 public class Main {
 
     public static void main(String[] args) {
