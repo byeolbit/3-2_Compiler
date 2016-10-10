0cadfd83421c8f618de1e16f09f3f9ad2c717d1c
Sang-Gyueong Jo <info@byeolbit.com>
2016-10-11 02:13:37 +0900

    Import ParseTree

--- ANTLR_test/src/ANTLRTest.java
+++ ANTLR_test/src/ANTLRTest.java
@@ -4,16 +4,15 @@
  */
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
 
-public class ANTLRTest
-{
+public class ANTLRTest {
     public static void main(String[] args) throws Exception
     {
-        MiniCLexer lexer = new MiniCLexer( new ANTLRFileStream(“test.c”));
-        CommonTokenStream tokens = new CommonTokenStream( lexer );
-        MiniCParser parser = new MiniCParser( tokens );
+        MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("dtest.c"));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MiniCParser parser = new MiniCParser(tokens);
         ParseTree tree = parser.program();
-
     }
 
 }
